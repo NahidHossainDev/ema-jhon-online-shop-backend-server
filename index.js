4833ff77307e8f433f7976200228158d6b7a4ec5
nahid9764 <nahidhossaain9764@gmail.com>
2020-10-08 15:54:01 +0600

    little change

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
+app.get("/", (req, res) => {
+  res.send("i am on")
+})
 
 const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b31bz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -60,4 +62,4 @@ client.connect((err) => {
 });
 
 
-app.listen(5000)
+app.listen(process.env.PORT || 5000)
